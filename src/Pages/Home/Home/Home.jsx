75e8a881f3ed08f59e3c78d103675c0946cfd5b2
developersharif1919 <developersharif1919@gmail.com>
2023-05-21 23:40:46 +0600

    Complete AOS Using In Home Page

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,21 +1,26 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import Newsletter from "../Newsletter/Newsletter";
 import Carousel from "../Carousel/Carousel";
 import ClientSay from "../ClientSay/ClientSay";
 import GallerySection from "../GalarySection/GallerySection";
 import ShopByCategory from "../ShopByCategory/ShopByCategory";
-import { useEffect } from "react";
+
 
 const Home = () => {
     useEffect(() => {
         document.title = "MDT | Home";
+        AOS.init(); // Initialize AOS
+        AOS.refresh(); // Refresh AOS when the content changes dynamically
     }, []);
     return (
         <div>
-            <Carousel></Carousel>
-            <GallerySection></GallerySection>
-            <ShopByCategory></ShopByCategory>
-            <ClientSay></ClientSay>
-            <Newsletter></Newsletter>
+            <Carousel data-aos="fade-up" />
+            <GallerySection data-aos="fade-up" />
+            <ShopByCategory data-aos="fade-up" />
+            <ClientSay data-aos="fade-up" />
+            <Newsletter data-aos="fade-up" />
         </div>
     );
 };
